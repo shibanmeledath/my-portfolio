fc54451292e2aac92fcaaa087733faea8778cda2
Shiban <2000.shiban@gmail.com>
2025-03-08 22:50:18 +0530

    Add new feature

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <>
     <footer className='bg-cyan-600 flex  justify-center py-2 '>
       <h1  className=''>Footer</h1> 
+      <p>Merge setup tesing</p>
       </footer>
     </>
   )
